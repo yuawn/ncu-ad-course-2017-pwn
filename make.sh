40512d8bfbe00429b2f80a7637e038392263b8e9
yuan <ssspeed00@gmail.com>
2017-10-04 18:29:05 +0800

    CTF

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd demo/home && make
-cd baby_bof/home && make
+cd ../../luck/home && make
+cd ../../baby_bof/home && make
+cd ../../shellcode/home && make
+cd ../../shellcode_revenge/home && make
