f21ef370ee8b4180cf8dcce47abab4e6ad964752
yuan <ssspeed00@gmail.com>
2017-10-05 09:44:47 +0800

    CTF

--- make.sh
+++ make.sh
@@ -4,3 +4,4 @@ cd ../../luck/home && make
 cd ../../baby_bof/home && make
 cd ../../shellcode/home && make
 cd ../../shellcode_revenge/home && make
+cd ../../rop/home && make
---
+++ rop/home/rop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+int main(){
+    setvbuf(stdout,0,2,0);
+    puts( "ROP attack is easy, isn't it? Show me your skill." );
+
+    char buf[0x20];
+    read( 0  , buf , 200 );
+
+    return 0;
+}
