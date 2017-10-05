a0e07c4cb0fe8d6e44eaff688520eab68b8340d4
yuan <ssspeed00@gmail.com>
2017-10-05 15:39:13 +0800

    CTF

--- ncu_center/home/ncu_center.c
+++ ncu_center/home/ncu_center.c
@@ -26,7 +26,7 @@ int main(){
 
     //char a[0x30] , b[0x30] , c[0x30];
     char s[3][0x30];
-    int a[3] , n;
+    int a[3] , n , i;
     a[0] = a[1] = a[2] = 0x30;
 
     while(1){
@@ -38,43 +38,43 @@ int main(){
                 break;
             case 2:
                 printf("Which one do you want to store in (1 , 2 , 3)?:");
-                scanf( "%d" , n );
-                if( n < 1 || n > 3 ){
+                scanf( "%d" , i );
+                if( i < 1 || i > 3 ){
                     puts( "Nop!" );
                     exit(0);
                 }
-                printf( "What do you want to store in mem page %d :" , n );
-                read( 0 , s[n] , 0x30 );
+                printf( "What do you want to store in mem page %d :" , i );
+                read( 0 , s[i] , 0x30 );
                 puts("done!");
                 break;
             case 3:
                 printf("Which memo page do you want to see (1 , 2 , 3)?:");
-                scanf( "%d" , n );
-                if( n < 1 || n > 3 ){
+                scanf( "%d" , i );
+                if( i < 1 || i > 3 ){
                     puts( "Nop!" );
                     exit(0);
                 }
-                if( strlen( s[n] ) < 1 ) {
+                if( strlen( s[i] ) < 1 ) {
                     puts("There is nothing in this memo page, please store something first.");
                     break;
                 }
-                printf( "memo page %d : %s" , n , s[n] );
+                printf( "memo page %d : %s" , n , s[i] );
                 break;
             case 4:
                 printf("Which memo page do you want to edit (1 , 2 , 3)?:");
-                scanf( "%d" , n );
-                if( n < 1 || n > 3 ){
+                scanf( "%d" , i );
+                if( i < 1 || i > 3 ){
                     puts( "Nop!" );
                     exit(0);
                 }
-                if( strlen( s[n] ) < 1 ) {
+                if( strlen( s[i] ) < 1 ) {
                     puts("There is nothing in this memo page, please store something first.");
                     break;
                 }
                 printf( "Edit memo page %d :" , n  );
-                read( 0 , s[n] , a[n] );
-                a[n] = strlen( s[n] );
-                printf( "done! new size : %d\n" , a[n] );
+                read( 0 , s[i] , a[i] );
+                a[i] = strlen( s[i] );
+                printf( "done! new size : %d\n" , a[i] );
                 break;
             case 5:
                 puts("Bye!");
