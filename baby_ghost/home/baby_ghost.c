1aa174f75aa3f494e7ef05d08ba1d4b16b224e2d
yuan <ssspeed00@gmail.com>
2017-11-16 14:45:40 +0800

    CTF

--- baby_ghost/home/baby_ghost.c
+++ baby_ghost/home/baby_ghost.c
@@ -38,8 +38,9 @@ void menu(){
     puts( "1. add a human" );
     puts( "2. modify a human" );
     puts( "3. add a ghost" );
-    puts( "4. show" );
-    puts( "5. exit" );
+    puts( "4. modify a ghost" );
+    puts( "5. show" );
+    puts( "6. exit" );
     puts( "---------------------------" );
     puts( "Your choice:" );
 }
@@ -51,6 +52,109 @@ int read_int(){
         _exit(1);
     }
     return atoi(buf);
+}
+
+struct human{
+    char dsc[0x100];
+    char *name;
+};
+
+struct ghost{
+    char dsc[0x18];
+    char *name;
+};
+
+human *h[10];
+ghost *g;
+
+void add_human(){
+
+    char buf[0x20];
+    memset( buf , 0 , 0x20 );
+
+    for( int i = 0 ; i < 10 ; ++i ){
+        if( !h[i] ){
+            h[i] = malloc( sizeof(h) );
+            memset( h[i]->dsc , 0 , 0x10 );
+            pritnf( "Say something :" );
+            read( 0 , h[i]->dsc , 0xff );
+            printf( "Oh! Your name :" );
+            read( 0 , buf , 0x1f );
+            h[i].name = strdup( buf );
+            puts( "done!" );
+            return;
+        }
+    }
+    puts( "Full!" );
+    return;
+}
+
+void add_ghost(){
+    if( !g ){
+        g = malloc( sizeof( ghost ) );
+        memset( g->dsc , 0 , 0x10 );
+        printf( "ghost name size:" )
+        int size = read_int();
+        char *tmp = NULL;
+        tmp = malloc( size );
+        if( !tmp ){
+            puts( "Alloc error" );
+            return;
+        }
+        g->name = tmp;
+        printf( "ghost name:" );
+        read( 0 , g->name , size );
+        puts( "You let me free, it must have some blood this night." );
+        return;
+    }
+    puts( "There is already a ghost!!!!! RUN!!!!!" );
+    exit(0);
+}
+
+void mod_human(){
+    char buf[0x20];
+    printf( "Index:" );
+    unsigned int idx = read_int() ;
+    if( idx > 9 ){
+        puts( "Nop!" );
+        exit(0);
+    }
+    if( !h[idx] ){
+        puts( "There is nobody." );
+        return;
+    }
+    printf( "New name:" );
+    read( 0 , buf, 0x1f );
+    h[i]->name = realloc( h[i]->name , strlen( buf ) );
+    strncpy( h[i]->name , buf , strlen( buf ) );
+    puts( "done!" );
+    return;
+}
+
+void mod_ghost(){
+    if( !g ){
+        puts( "Wooooooooo~~~ miss me?" );
+        return;
+    }
+    else if( g = 0x9487 ){
+        puts( "I got you WA HA HA." );
+        exit(0);
+    }
+    printf( "New name of ghost :" );
+    read( 0 , g->name , 2 );
+    g = 0x9487;
+    puts( "OH NO!! Im cursed......" );
+    return;
+}
+
+void show(){
+    for( int i = 0 ; i < 10 ; ++i ){
+        if( h[i] ){
+            printf( "%s: %s\n", h[i]->name , h[i]->dsc  );
+        }
+    }
+    puts( "You can't see the ghost, find it!" );
+    return;
 }
 
 int main(){
@@ -66,18 +170,20 @@ int main(){
         n = read_int();
         switch( n ){
             case 1:
-
+                add_human();
                 break;
             case 2:
-                
+                mod_human();
                 break;
             case 3:
-                
+                add_ghost();
                 break;
             case 4:
-                
+                mod_ghost();
                 break;
             case 5:
+                show();
+            case 6:
                 puts("Bye!");
                 return 0;
             default:
